6476e31976f84b845e2ebb7682f33aabb2d9f2a0
Carlos Paredes <cparedesdevpi@gmail.com>
2024-03-21 17:27:48 -0600

    [ADD] - creating project

---
+++ TMDB App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TMDB App
+//
+//  Created by Carlos Paredes on 21/3/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
